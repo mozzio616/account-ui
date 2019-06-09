1eb336cf849ddb65fa93afaeabc94b3a41da9137
mozzio <mozzio@halnoMacBook.local>
2019-06-09 23:39:23 +0900

    dynamic table for payment methods

--- routes/paymentmethods.js
+++ routes/paymentmethods.js
@@ -1,16 +1,35 @@
 'use strict'
 
 const router = require('express').Router();
+const config = require('config');
+process.env["NODE_CONFIG_DIR"] = __dirname + '/../config/';
+const rp = require('request-promise');
 
 router.get('/', (req, res) => {
 
   const username = req.session.user.username;
-  const data = {
-    username: username,
-    displayUsername: username.split('@')[0]
-  };
 
-  res.render('./paymentmethods.ejs', data);
+  const options = {
+    method: 'GET',
+    url: config.api.accounts + '/' + username + '/paymentmethods'
+  }
+
+  rp(options)
+    .then(function(body) {
+      const paymentMethods = JSON.parse(body).paymentMethods;
+      const data = {
+        username: username,
+        displayUsername: username.split('@')[0],
+        paymentMethods: paymentMethods
+      };
+      res.render('./paymentmethods.ejs', data);
+    })
+    .catch(function(err) {
+      console.log('error occurred calling api');
+      console.log(err.statusCode);
+      res.status(err.statusCode);
+      res.render('./paymentmethods.ejs');
+    })
 
 });
 
